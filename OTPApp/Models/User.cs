ae58fb3e4c5c55c51e3c3dd3e7acfabb533184fd
JamesTsui <tsuisauchi.tsc@gmail.com>
2023-10-10 23:58:29 +0800

    Simplify data model

--- OTPApp/Models/User.cs
+++ OTPApp/Models/User.cs
@@ -7,17 +7,10 @@ namespace OTPApp.Models
 {
     public partial class User
     {
-        public User()
-        {
-            Otps = new HashSet<Otp>();
-        }
-
         public int Id { get; set; }
-        public int? StatusId { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-
-        public virtual UserStatus Status { get; set; }
-        public virtual ICollection<Otp> Otps { get; set; }
+        public string Otp { get; set; }
+        public int? Attempt { get; set; }
     }
 }
