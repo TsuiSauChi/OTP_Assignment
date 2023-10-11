faf95a1f6f00fe968e5f54f614470c9c39e6ebeb
JamesTsui <tsuisauchi.tsc@gmail.com>
2023-10-11 11:35:39 +0800

    Fix Postgresql Connection

--- OTPApp/Data/DataContext.cs
+++ OTPApp/Data/DataContext.cs
@@ -25,7 +25,7 @@ namespace OTPApp.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseNpgsql("Host=172.19.0.2;Database=postgres;Username=postgres;Password=example");
+                optionsBuilder.UseNpgsql("Host=172.20.0.2;Database=postgres;Username=postgres;Password=example");
             }
         }
 
